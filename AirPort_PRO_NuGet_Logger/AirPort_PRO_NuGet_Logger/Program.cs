e43758e15fc0a010c06bbe8ddc4a3f64f9fef151
123rjwq <127960752+123kir@users.noreply.github.com>
2024-11-22 22:56:21 +0300

    1
    загрузил старую версию

---
+++ AirPort_PRO_NuGet_Logger/AirPort_PRO_NuGet_Logger/Program.cs
@@ -0,0 +1,37 @@
+﻿using AirPort_PRO_NuGet_Logger.AirPortManager;
+using DataGridAirPort.Storage.Memory;
+using Serilog;
+using Serilog.Extensions.Logging;
+using System;
+using System.Windows.Forms;
+
+namespace AirPort_PRO_NuGet_Logger
+{
+    /// <summary>
+    /// Главный класс приложения, содержащий точку входа
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var serilogLogger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Seq("http://localhost:5341", apiKey: "")
+                .CreateLogger();
+
+            var logger = new SerilogLoggerFactory(serilogLogger).CreateLogger("DataGrid");
+
+            var storage = new MemoryAirPlaneStorage();
+            var manager = new PlaneManager_cs(storage, logger);
+
+            Application.Run(new Form1(manager));
+        }
+    }
+}
